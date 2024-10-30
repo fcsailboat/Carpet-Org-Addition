dc2146dd47c9cf2369add87b0bc317884edf97fe
cdqtzrc <3286637847@qq.com>
2024-10-31 01:14:28 +0800

    更新抑制潜影盒在被发射器放置时移除自定义名称

---
+++ src/main/java/org/carpetorgaddition/rule/RuleUtils.java
@@ -0,0 +1,29 @@
+package org.carpetorgaddition.rule;
+
+import org.carpetorgaddition.CarpetOrgAdditionSettings;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class RuleUtils {
+
+    /**
+     * 潜影盒是否可以触发更新抑制器
+     *
+     * @param world 潜影盒所在维度
+     * @param pos   潜影盒的位置
+     */
+    public static boolean canUpdateSuppression(@Nullable String blockName) {
+        if ("false".equalsIgnoreCase(CarpetOrgAdditionSettings.CCEUpdateSuppression)) {
+            return false;
+        }
+        if (blockName == null) {
+            return false;
+        }
+        if ("true".equalsIgnoreCase(CarpetOrgAdditionSettings.CCEUpdateSuppression)) {
+            return "更新抑制器".equals(blockName) || "updateSuppression".equalsIgnoreCase(blockName);
+        }
+        // 比较字符串并忽略大小写
+        return Objects.equals(CarpetOrgAdditionSettings.CCEUpdateSuppression.toLowerCase(), blockName.toLowerCase());
+    }
+}
