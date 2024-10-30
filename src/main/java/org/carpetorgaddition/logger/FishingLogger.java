3b6d900f526ff072d327ee04f0234c738508bc0c
cdqtzrc <3286637847@qq.com>
2024-10-30 19:47:48 +0800

    钓鱼记录器

--- src/main/java/org/carpetorgaddition/logger/FishingLogger.java
+++
@@ -1,9 +0,0 @@
-package org.carpetorgaddition.logger;
-
-/**
- * 钓鱼指示记录器
- */
-public class FishingLogger {
-    private static final String LOGGER_NAME = "fishing";
-    public static boolean fishing = false;
-}
---
+++ src/main/java/org/carpetorgaddition/mixin/logger/FishingBobberEntityMixin.java
@@ -0,0 +1,52 @@
+package org.carpetorgaddition.mixin.logger;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.text.MutableText;
+import net.minecraft.util.Formatting;
+import org.carpetorgaddition.logger.Loggers;
+import org.carpetorgaddition.util.MessageUtils;
+import org.carpetorgaddition.util.TextUtils;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FishingBobberEntity.class)
+public abstract class FishingBobberEntityMixin {
+    @Shadow
+    @Nullable
+    public abstract PlayerEntity getPlayerOwner();
+
+    @Shadow
+    private int hookCountdown;
+
+    @Shadow
+    private int waitCountdown;
+
+    @Shadow
+    private int fishTravelCountdown;
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void tick(CallbackInfo ci) {
+        if (Loggers.fishing) {
+            PlayerEntity player = this.getPlayerOwner();
+            if (player == null) {
+                return;
+            }
+            if (this.waitCountdown != 0) {
+                // 鱼出现
+                MessageUtils.sendMessageToHud(player, TextUtils.translate("carpet.logger.fishing.appear", this.waitCountdown));
+            } else if (this.fishTravelCountdown != 0) {
+                // 鱼上钩
+                MessageUtils.sendMessageToHud(player, TextUtils.translate("carpet.logger.fishing.bite", this.fishTravelCountdown));
+            } else if (this.hookCountdown != 0) {
+                // 鱼挣脱
+                MutableText translate = TextUtils.translate("carpet.logger.fishing.break_free", this.hookCountdown);
+                MessageUtils.sendMessageToHud(player, TextUtils.setColor(translate, Formatting.GREEN));
+            }
+        }
+    }
+}
