15045c0cc6d8fcce515cfc793fa745aaf1b22a89
fcsailboat <3286637847@qq.com>
2025-07-19 23:37:57 +0800

    根据文件名获取最新文件

--- src/build_jar.py
+++ src/build_jar.py
@@ -1,7 +1,8 @@
 import os
+import re
 import shutil
 import subprocess
-from subprocess import TimeoutExpired
+from subprocess import TimeoutExpired, CompletedProcess
 
 from git import Repo
 
@@ -14,32 +15,65 @@ version_count = 0
 repo = Repo(directory)
 
 
-def copy_the_latest_file():
+def latest_file(files: list[str], branch: str):
+    """获取最新的模组文件
+    :param files:
+    :param branch:
+    :return:最新构建的模组文件的绝对路径
+    """
+    jar_files = [file for file in files if is_jar_file(branch, file)]
+    return max(jar_files, key=str)
+
+
+def is_jar_file(branch: str, file: str):
+    """
+    指定文件是否为当前分支的模组文件
+    :param branch:Git分支的名称
+    :param file:文件的绝对路径
+    """
+    return re.match(f"carpet-org-addition-mc{branch}-v.*-\\d+\\.jar", os.path.basename(file))
+
+
+def copy_the_latest_file(branch: str):
+    """
+    将最新构建的文件复制到version文件夹
+    :param branch:
+    """
     global directory, version_path
     path = os.path.join(directory, "build\\libs")
-    files = [os.path.join(path, file) for file in os.listdir(path) if file not in "source"]
-    last = max(files, key=os.path.getmtime)
+    files = [os.path.join(path, file) for file in os.listdir(path)]
+    last = latest_file(files, branch)
     print(f"正在将文件{os.path.basename(last)}复制到{path}")
     shutil.copy(last, version_path)
 
 
-def build_jar():
+def build_jar(branch: str):
     """
     构建当前分支的模组jar
     """
     count = 0
     while True:
+        if count >= 3:
+            raise RuntimeError("重试次数过多")
         count += 1
         try:
             print(f"正在进行第{count}次尝试")
-            subprocess.run(args=["gradlew", "remapJar"], timeout=600, cwd=directory, shell=True)
-            copy_the_latest_file()
-            global version_count
-            version_count += 1
-        except TimeoutExpired as e:
+            result: CompletedProcess[bytes] = subprocess.run(
+                args=["gradlew", "remapJar"],
+                timeout=600,
+                cwd=directory,
+                shell=True
+            )
+            return_code = result.returncode
+            if return_code == 0:
+                copy_the_latest_file(branch)
+                global version_count
+                version_count += 1
+            else:
+                print(f"第{count}次构建{branch}失败，返回码：{return_code}")
+                continue
+        except TimeoutExpired:
             print("操作超时，正在重试")
-            if count >= 3:
-                raise e
             continue
         break
     print("完成构建")
@@ -62,7 +96,7 @@ def build_all_version_jar():
         print(f"正在切换到{branch}分支")
         switch_branch(branch)
         print(f"正在构建{branch}版本")
-        build_jar()
+        build_jar(branch)
         print("-" * 70)
     global version_count
     if version_count != len(branches):
