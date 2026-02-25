f7cf8dc27fc58dd1d8eacc7d38b7bdb58f020d67
fcsailboat <3286637847@qq.com>
2026-02-25 12:17:46 +0800

    升级到26.1-snapshot-10

--- src/main/java/boat/carpetorgaddition/mixin/rule/channelingignoreweather/BlockAndTintGetterMixin.java
+++ src/main/java/boat/carpetorgaddition/mixin/rule/channelingignoreweather/BlockAndTintGetterMixin.java
@@ -2,13 +2,13 @@ package boat.carpetorgaddition.mixin.rule.channelingignoreweather;
 
 import boat.carpetorgaddition.CarpetOrgAdditionSettings;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.BlockAndLightGetter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlockAndTintGetter.class)
+@Mixin(BlockAndLightGetter.class)
 public interface BlockAndTintGetterMixin {
     @Inject(method = "canSeeSky", at = @At("HEAD"), cancellable = true)
     private void canSeeSky(BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
