065f0a043a9a128219757e12a6dccc3610707a33
fcsailboat <3286637847@qq.com>
2026-02-26 11:20:58 +0800

    调整物品堆计数器实现

---
+++ src/main/java/boat/carpetorgaddition/wheel/ItemIdentity.java
@@ -0,0 +1,65 @@
+package boat.carpetorgaddition.wheel;
+
+import net.minecraft.core.component.DataComponentPatch;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ItemStackTemplate;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
+
+public final class ItemIdentity {
+    private final Item item;
+    private final DataComponentPatch components;
+    @Nullable
+    private ItemStack itemStack;
+
+    public ItemIdentity(Item item, DataComponentPatch components) {
+        this.item = item;
+        this.components = components;
+    }
+
+    public ItemIdentity(ItemStack itemStack) {
+        this(itemStack.getItem(), itemStack.getComponentsPatch());
+        this.itemStack = itemStack;
+    }
+
+    @SuppressWarnings("unused")
+    public ItemIdentity(ItemStackTemplate template) {
+        this(template.item().value(), template.components());
+    }
+
+    @NonNull
+    public ItemStack asItemStack() {
+        if (this.itemStack == null) {
+            this.itemStack = new ItemStackTemplate(this.item, this.getComponents()).create();
+        }
+        return this.itemStack;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public DataComponentPatch getComponents() {
+        return components;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (ItemIdentity) obj;
+        return Objects.equals(this.item, that.item) && Objects.equals(this.components, that.components);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item, components);
+    }
+}
--- src/main/java/boat/carpetorgaddition/wheel/ItemStackCounter.java
+++ src/main/java/boat/carpetorgaddition/wheel/ItemStackCounter.java
@@ -1,6 +1,5 @@
 package boat.carpetorgaddition.wheel;
 
-import boat.carpetorgaddition.util.InventoryUtils;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.minecraft.world.item.ItemStack;
 import org.jspecify.annotations.NonNull;
@@ -11,7 +10,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ItemStackCounter implements Counter<ItemStack> {
-    private final Counter<ItemStackWrapper> counter = new SimpleCounter<>();
+    private final Counter<ItemIdentity> counter = new SimpleCounter<>();
 
     public ItemStackCounter() {
     }
@@ -22,17 +21,17 @@ public class ItemStackCounter implements Counter<ItemStack> {
 
     @Override
     public void add(ItemStack itemStack, int count) {
-        this.counter.add(new ItemStackWrapper(itemStack), count);
+        this.counter.add(new ItemIdentity(itemStack), count);
     }
 
     @Override
     public void set(ItemStack itemStack, int count) {
-        this.counter.set(new ItemStackWrapper(itemStack), count);
+        this.counter.set(new ItemIdentity(itemStack), count);
     }
 
     @Override
     public int getCount(ItemStack itemStack) {
-        return this.counter.getCount(new ItemStackWrapper(itemStack));
+        return this.counter.getCount(new ItemIdentity(itemStack));
     }
 
     @Override
@@ -47,37 +46,21 @@ public class ItemStackCounter implements Counter<ItemStack> {
 
     @Override
     public Stream<Object2IntMap.Entry<ItemStack>> stream() {
-        return this.counter.stream().map(entry -> Object2IntMap.entry(entry.getKey().itemStack(), entry.getIntValue()));
+        return this.counter.stream().map(entry -> Object2IntMap.entry(entry.getKey().asItemStack(), entry.getIntValue()));
     }
 
     @Override
     public Set<ItemStack> keySet() {
-        return this.counter.keySet().stream().map(ItemStackWrapper::itemStack).collect(Collectors.toSet());
+        return this.counter.keySet().stream().map(ItemIdentity::asItemStack).collect(Collectors.toSet());
     }
 
     @Override
     public Set<Object2IntMap.Entry<ItemStack>> entrySet() {
-        return this.counter.stream().map(entry -> Object2IntMap.entry(entry.getKey().itemStack(), entry.getIntValue())).collect(Collectors.toSet());
+        return this.counter.stream().map(entry -> Object2IntMap.entry(entry.getKey().asItemStack(), entry.getIntValue())).collect(Collectors.toSet());
     }
 
     @Override
     public @NonNull Iterator<ItemStack> iterator() {
-        return this.counter.stream().map(Object2IntMap.Entry::getKey).map(ItemStackWrapper::itemStack).toList().iterator();
-    }
-
-    public record ItemStackWrapper(ItemStack itemStack) {
-        @Override
-        public boolean equals(Object o) {
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            ItemStackWrapper wrapper = (ItemStackWrapper) o;
-            return InventoryUtils.canMerge(this.itemStack, wrapper.itemStack);
-        }
-
-        @Override
-        public int hashCode() {
-            return ItemStack.hashItemAndComponents(itemStack);
-        }
+        return this.counter.stream().map(Object2IntMap.Entry::getKey).map(ItemIdentity::asItemStack).iterator();
     }
 }
