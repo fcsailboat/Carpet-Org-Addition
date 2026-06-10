edd91132d3ff91b2f86a3cc283c76a0a342aace9
fcsailboat <3286637847@qq.com>
2026-06-10 16:43:22 +0800

    更新翻译

--- src/main/java/boat/carpetorgaddition/client/CarpetOrgAdditionClient.java
+++ src/main/java/boat/carpetorgaddition/client/CarpetOrgAdditionClient.java
@@ -7,6 +7,8 @@ import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.KeyMapping;
 
+import java.util.List;
+
 public class CarpetOrgAdditionClient implements ClientModInitializer {
     /**
      * 清除高亮路径点的按键绑定
@@ -24,16 +26,25 @@ public class CarpetOrgAdditionClient implements ClientModInitializer {
 
     private void logWittyComment() {
         String name = ClientUtils.getSession().getName();
-        String massage = switch (name) {
-            case "qscfthmko099" -> "How did we get here?";
-            case "half_kite" -> "Happy birthday!";
-            case "MR_LAGANXIANG" -> "You're back!!!";
-            case "qweryyuoskv" -> "The bomb been planted!";
-            case "zhaixianyu" -> "Why don't you ever take advice?";
-            default -> null;
+        List<String> messages = switch (name) {
+            case "qscfthmko099" -> List.of("How did we get here?");
+            case "half_kite" -> List.of("Happy birthday!");
+            case "MR_LAGANXIANG" -> List.of(
+                    // 矿艺：Minecraft的文言译名为“矿艺大典”
+                    // https://lzh.minecraft.wiki/w/%E7%A4%A6%E8%97%9D%E5%A4%A7%E5%85%B8
+                    "半世萍踪各西东，矿艺成坏两匆匆。",
+                    "故纸浮沉同晓梦，异乡风雨误归鸿。",
+                    // 南溟：南边的大海，但在这里指澳大利亚
+                    "南溟苦旅霜欺鬓，小劫随身笑倚风。",
+                    "倦看聚散寻常事，归来独卧雨声中。"
+            );
+            // 这并非出自《CS:GO》，而是他学Java时写的一个数字炸弹，语法瑕疵是有意为之
+            case "qweryyuoskv" -> List.of("The bomb been planted!");
+            case "zhaixianyu" -> List.of("Why don't you ever take advice?");
+            default -> List.of();
         };
-        if (massage != null) {
-            CarpetOrgAddition.LOGGER.info(massage);
+        for (String message : messages) {
+            CarpetOrgAddition.LOGGER.info(message);
         }
     }
 }
